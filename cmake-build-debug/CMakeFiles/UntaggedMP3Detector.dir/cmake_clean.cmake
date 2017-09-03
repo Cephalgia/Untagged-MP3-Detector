eb8a6e3a7866ff28afa361eaab4b788030b6a094
Unknown <eternalcephalalgia@gmail.com>
2017-09-03 12:51:32 +0300

    Initial commit: searching for mp3's, detecting those without tags

---
+++ cmake-build-debug/CMakeFiles/UntaggedMP3Detector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UntaggedMP3Detector.dir/main.cpp.o"
+  "UntaggedMP3Detector.pdb"
+  "UntaggedMP3Detector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UntaggedMP3Detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
